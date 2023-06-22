2d3a07e7e136cf41856ba863f629827195477a6d
felixNyalenda <felixasejunior@gmail.com>
2023-06-22 13:02:37 +0300

    Completed assignment

--- app/controllers/dog_houses_controller.rb
+++ app/controllers/dog_houses_controller.rb
@@ -1,9 +1,13 @@
 class DogHousesController < ApplicationController
   rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
 
+  def index
+    dogs_houses = DogHouse.all
+    render json: dogs_houses, include: {reviews: {except: [:updated_at, :created_at]}}, except: [:updated_at, :created_at], status: :ok
+  end
   def show
     dog_house = DogHouse.find(params[:id])
-    render json: dog_house, include: :reviews
+    render json: dog_house, include: {reviews: {except: [:updated_at, :created_at]} }, except: [:updated_at, :created_at], status: :ok
   end
 
   private
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,7 +2,7 @@ class ReviewsController < ApplicationController
 
   def index
     reviews = Review.all.order(rating: :desc)
-    render json: reviews, include: :dog_house
+    render json: reviews, include: { dog_house: {except: [:updated_at, :created_at]} }, except: [:updated_at, :created_at], status: :ok
   end
 
 end
